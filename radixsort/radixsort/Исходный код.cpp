20f0db7beb14fabff9fec5ad52c4103b49b8e376
gooldan <egor5700@ya.ru>
2014-03-05 00:21:59 +0400

    fixed
    there aren't any memory problems now;
    sorts correctly

--- radixsort/radixsort/Исходный код.cpp
+++ radixsort/radixsort/Исходный код.cpp
@@ -5,15 +5,20 @@ using namespace std;
 void RadixSort(int a[], int len)
 {
 	int **bas= new int*[10];
-	for (int i = 0; i < len; ++i)
+	for (int i = 0; i < 10; ++i)
 		bas[i] = new int[len];
 	bool working = true;
-	int *ind=new int[len];
-
+	int *ind=new int [10];
+	
+	bool stillw = true;
 	int lev = 0;
 	while (working)
 	{
-		
+		for (int i = 0; i < 10; ++i)
+		{
+			ind[i] = 0;
+		}
+		stillw = true;
 		for (int i = 0; i < len; ++i)
 		{
 			int ln = 0;
@@ -21,13 +26,15 @@ void RadixSort(int a[], int len)
 			for (int j = 0; j < lev; ++j)
 				a[i] /= 10;
 			ln = a[i] % 10;
-			if (ln == 0) working = false;
+			if (stillw && ln == 0) working = false;
 			else
+			{
+				stillw = false;
 				working = true;
+			}
 			a[i] = t;
-			ind[i] = 0;
 		}
-		
+		if (!working) break;
 		for (int i = 0; i < len; ++i)
 		{
 			int ln = 0;
@@ -35,28 +42,30 @@ void RadixSort(int a[], int len)
 			for (int j = 0; j < lev; ++j)
 				t /= 10;
 			ln = t % 10;
-			bas[ln][ind[ln]++] = a[i];
+			bas[ln][ind[ln]] = a[i];
+			ind[ln]++;
 		}
 		int k = 0;
 		for (int i = 0; i < 10; ++i)
 		{
 			for (int j = 0; j < ind[i]; ++j)
 			{
-				a[k++] = bas[i][j];
+				a[k] = bas[i][j];
 				bas[i][j] = 0;
+				k++;
 			}
 		}
 		lev++;
 	}
-	for (int k = 0; k < len; k++)
+	for (int k = 0; k < 10; k++)
 		delete[] bas[k];
-	//delete[] bas;
-
+	delete[] bas;
+	delete[] ind;
 	
 }
 int main()
 {
-	int a[14] = { 12, 6, 27, 2, 5, 222, 311, 14, 127, 8, 16263, 13, 28, 17 };
+	int a[14] = { 12, 6, 27, 2, 5, 222, 311, 14, 127, 8, 16263, 13, 28, 0 };//
 	RadixSort(a, 14);
 	int i;
 	for (int i = 0; i < 14; ++i)
